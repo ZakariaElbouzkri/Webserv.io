3bc356d82ff9bb723afbad6fcf36d77c4d41cd0a
Zakaria El Bouzkri <zel-bouz@e1r8p3.1337.ma>
2023-12-23 12:47:20 +0100

    setting up the project

---
+++ includes/servIO.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <iostream>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <poll.h>
+#include <sys/select.h>
+#include <vector>
+#include <map>
+#include <utility>
+
+#define CONF_PATH "./configuration/servIO.conf"
+
