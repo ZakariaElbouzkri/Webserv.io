42416ee3bbbc48d63ab0deada461550bc2863f29
Zakaria El Bouzkri <zel-bouz@e1r4p3.1337.ma>
2024-01-02 21:09:44 +0100

    update ostream operator overload:

--- parser/Token.cpp
+++ parser/Token.cpp
@@ -6,7 +6,7 @@
 /*   By: zel-bouz <zel-bouz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/29 21:42:12 by zel-bouz          #+#    #+#             */
-/*   Updated: 2023/12/30 05:50:15 by zel-bouz         ###   ########.fr       */
+/*   Updated: 2024/01/02 20:49:17 by zel-bouz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@ bool	Token::operator!=( token_t rhs ) {
 }
 
 bool	Token::isKey( void ) const {
-	return __type >= HTTP && __type <= ROOT;
+	return __type >= HTTP && __type <= CGI;
 }
 
 Token::token_t	Token::getType( void ) const {
@@ -40,10 +40,10 @@ const std::string&	Token::getData( void ) const {
 
 std::ostream&	operator<<( std::ostream& os, const Token& rhs ) {
 	std::string	keys[21] = {
-		"WORD", "OPEN_CURLY", "CLOSE_CURLY", "OPEN_SQUARE", "CLOSE_SQUARE", "SIMICOLEN",
-		"HTTP", "SERVER", "LOCATION", "ERR_PAGE", "ERR_LOG", "ACC_LOG", "ALLOW", "MAX_BODY",
-		"INDEX", "AUTOINDEX", "RETURN", "LISTEN", "ROOT", "CGI", "_EOF",
+		"value " + rhs.__data  , "{", "}", "[", "]", ";", "http", "server", "location",
+		"error_page", "error_log", "access_log", "allow", "client_max_body_size",
+		"index", "autoindex", "redirect", "listen", "root", "cgi", "_EOF",
 	};
-	os << keys[(int)rhs.__type] << " " << rhs.__data;
+	os << keys[(int)rhs.__type] << " " << rhs.__data << (rhs.isKey() ? "keyword" : "" );
 	return os;
 }
